d2380e43d4771cb6e2d8af74261745eefe16b826
Lucas Neves <lucas.neves12@icloud.com>
2023-10-25 15:40:26 -0300

    Implementing scrollView and now button for new goals is working, but still have problems with the scrollView and need to refactor the code later.

--- UpYou/UpYou/Features/Finance Section/Sections/Goals/GoalsVC.swift
+++ UpYou/UpYou/Features/Finance Section/Sections/Goals/GoalsVC.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class GoalsVC: UIViewController {
     
+    private var currentYPosition: CGFloat = 20
+    
     private lazy var backButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -36,6 +38,14 @@ class GoalsVC: UIViewController {
         label.text = "Metas"
         label.font = UIFont.boldSystemFont(ofSize: 30)
         return label
+    }()
+    
+    private lazy var infoContainer: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.isScrollEnabled = true
+        scrollView.contentSize = CGSize(width: scrollView.bounds.size.width, height: scrollView.bounds.size.height)
+        return scrollView
     }()
     
     private lazy var addGoalButton: UIButton = {
@@ -78,8 +88,9 @@ class GoalsVC: UIViewController {
     }
     
     private func createInfoBox(goal: String, value: String, time: String) {
+        print("Creating info box with: Goal: \(goal), Value: \(value), Time: \(time)")
         
-      lazy var viewBackground: UIView = {
+        lazy var viewBackground: UIView = {
             let view = UIView()
             view.translatesAutoresizingMaskIntoConstraints = false
             view.backgroundColor = .black.withAlphaComponent(0.6)
@@ -88,36 +99,71 @@ class GoalsVC: UIViewController {
             return view
         }()
         
-        lazy var stack: UIStackView = {
-            let variable = UIStackView()
-            variable.translatesAutoresizingMaskIntoConstraints = false
-            variable.distribution = .fillEqually
-            variable.axis = .vertical
-            variable.spacing = 0.5
-            return variable
+        lazy var infoImageBox: UIImageView = {
+            let image = UIImageView(image: UIImage.setAgoalIcon)
+            image.translatesAutoresizingMaskIntoConstraints = false
+            image.clipsToBounds = true
+            image.layer.cornerRadius = 18
+            return image
         }()
         
-        lazy var infoBox: CustomInfoBox = {
-            let component = CustomInfoBox(image: UIImage.setAgoalIcon, title: goal, value$: value, time$: time)
-            component.translatesAutoresizingMaskIntoConstraints = false
-            return component
+        lazy var goalLabel: UILabel = {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.text = "Meta: \(goal)"
+            label.textColor = .white
+            label.font = UIFont.systemFont(ofSize: 14)
+            return label
+        }()
+        
+        lazy var valueLabel: UILabel = {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.textColor = .white
+            label.text = "Valor: \(value)"
+            label.font = UIFont.systemFont(ofSize: 14)
+            return label
+        }()
+        
+        lazy var timeLabel: UILabel = {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.textColor = .white
+            label.text = "Tempo: \(time)"
+            label.font = UIFont.systemFont(ofSize: 14)
+            return label
         }()
         
         view.addSubview(viewBackground)
-        viewBackground.addSubview(stack)
-        stack.addArrangedSubview(infoBox)
+        viewBackground.addSubview(infoImageBox)
+        viewBackground.addSubview(goalLabel)
+        viewBackground.addSubview(valueLabel)
+        viewBackground.addSubview(timeLabel)
         
         NSLayoutConstraint.activate([
-            viewBackground.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 120),
-            viewBackground.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            viewBackground.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            viewBackground.heightAnchor.constraint(equalToConstant: 480),
+            viewBackground.topAnchor.constraint(equalTo: infoContainer.topAnchor, constant: currentYPosition),
+            viewBackground.leadingAnchor.constraint(equalTo: infoContainer.leadingAnchor, constant: 20),
+            viewBackground.trailingAnchor.constraint(equalTo: infoContainer.trailingAnchor, constant: -20),
+            viewBackground.heightAnchor.constraint(equalToConstant: 120),
             
-            stack.topAnchor.constraint(equalTo: viewBackground.topAnchor),
-            stack.leadingAnchor.constraint(equalTo: viewBackground.leadingAnchor),
-            stack.trailingAnchor.constraint(equalTo: viewBackground.trailingAnchor),
-            stack.bottomAnchor.constraint(equalTo: viewBackground.bottomAnchor, constant: 20), 
+            infoImageBox.centerYAnchor.constraint(equalTo: viewBackground.centerYAnchor),
+            infoImageBox.leadingAnchor.constraint(equalTo: viewBackground.leadingAnchor, constant: 12),
+            infoImageBox.widthAnchor.constraint(equalToConstant: 60),
+            infoImageBox.heightAnchor.constraint(equalToConstant: 60),
+            
+            goalLabel.topAnchor.constraint(equalTo: infoImageBox.topAnchor, constant: 10),
+            goalLabel.leadingAnchor.constraint(equalTo: infoImageBox.trailingAnchor, constant: 10),
+            
+            valueLabel.topAnchor.constraint(equalTo: goalLabel.bottomAnchor, constant: 8),
+            valueLabel.leadingAnchor.constraint(equalTo: goalLabel.leadingAnchor),
+            
+            timeLabel.topAnchor.constraint(equalTo: valueLabel.bottomAnchor, constant: 5),
+            timeLabel.trailingAnchor.constraint(equalTo: viewBackground.trailingAnchor, constant: -12),
         ])
+        
+        currentYPosition += 140
+        
+        infoContainer.contentSize = CGSize(width: infoContainer.frame.width, height: currentYPosition)
     }
     
     override func viewDidLoad() {
@@ -129,6 +175,7 @@ class GoalsVC: UIViewController {
         view.addSubview(subImageView)
         view.addSubview(backButton)
         view.addSubview(titleLabel)
+        view.addSubview(infoContainer)
         view.addSubview(addGoalButton)
     }
     
@@ -146,11 +193,15 @@ class GoalsVC: UIViewController {
             titleLabel.topAnchor.constraint(equalTo: backButton.bottomAnchor, constant: 20),
             titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             
+            infoContainer.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            infoContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            infoContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            infoContainer.bottomAnchor.constraint(equalTo: addGoalButton.topAnchor, constant: -20),
+            
             addGoalButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 25),
             addGoalButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -25),
             addGoalButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -40),
             addGoalButton.heightAnchor.constraint(equalToConstant: 50),
-            
         ])
     }
 }
